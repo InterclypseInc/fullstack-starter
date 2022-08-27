96c7d08a8e172298cfb228f5df01c17bf12d85d3
Benjamin Mason <ben@nwstrtrj01.rd.lv.cox.cci>
2022-08-27 01:13:38 -0400

    Add Create Inventory Controller & JUnit Test

--- starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
+++ starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
@@ -3,8 +3,11 @@ package com.starter.fullstack.rest;
 import com.starter.fullstack.api.Inventory;
 import com.starter.fullstack.dao.InventoryDAO;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Inventory Controller.
  */
 @RestController
-@RequestMapping("/inventory")
+@RequestMapping("/inventories")
 public class InventoryController {
   private final InventoryDAO inventoryDAO;
 
@@ -26,12 +29,24 @@ public class InventoryController {
   }
 
   /**
-   * Find Products.
-   * @return List of Product.
+   * Find Inventories.
+   * @return List of Inventory.
    */
   @GetMapping
   public List<Inventory> findInventories() {
     return this.inventoryDAO.findAll();
   }
+
+  /**
+   * Create Inventory.
+   *
+   * @param inventory inventory.
+   * @return Inventory.
+   */
+  @PostMapping
+  public Inventory createInventory(@Valid @RequestBody Inventory inventory) {
+    return this.inventoryDAO.create(inventory);
+  }
 }
+
 

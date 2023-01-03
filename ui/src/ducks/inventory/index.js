1d9b6a382b405f0dca24f4f090888846fe875a62
michel0323 <87438059+michel0323@users.noreply.github.com>
2023-01-03 01:37:40 -0500

    First JavaScript File

--- ui/src/ducks/inventory/index.js
+++ ui/src/ducks/inventory/index.js
@@ -1,17 +1,30 @@
-import { createAction, handleActions } from 'redux-actions'
+import { createAction, handleActions } from 'redux-actions';
 
 const actions = {
-  INVENTORY_GET_ALL: 'inventory/get_all',
-}
+	INVENTORY_GET_ALL: 'inventory/get_all',
+};
 
 export let defaultState = {
-  all: []
-}
+	all: [],
+};
 
 export const findInventory = createAction(actions.INVENTORY_GET_ALL, () => {
-  //TODO
-})
+	//Last modified by Michel T. On 01/03/23
+	(dispatch, getState, config) =>
+		axios
+			.get(`${config.restAPIUrl}/inventory`)
+			.then((suc) => dispatch(refreshInventory(suc.data)));
+});
 
-export default handleActions({
-  //TODO
-}, defaultState)
+export default handleActions(
+	{
+		[actions.INVENTORY_GET_ALL]: (state, action) => {
+			return {
+				...state,
+				all: action.payload,
+			};
+		},
+	},
+
+	defaultState
+);

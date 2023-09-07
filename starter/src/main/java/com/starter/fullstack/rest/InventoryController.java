f3ba3a4c3ea7d6e657423c5bf355399355b37413
KennyMcD <kennymcdonnell1997@gmail.com>
2023-09-07 18:54:56 -0400

    First attempt task 2

--- starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
+++ starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
@@ -5,6 +5,8 @@ import com.starter.fullstack.dao.InventoryDAO;
 import java.util.List;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,16 @@ public class InventoryController {
   public InventoryController(InventoryDAO inventoryDAO) {
     Assert.notNull(inventoryDAO, "Inventory DAO must not be null.");
     this.inventoryDAO = inventoryDAO;
+  }
+
+  /**
+   * Save Inventory.
+   * @param inventory Inventory to Save/Update.
+   * @return Created/Updated Inventory.
+   */
+  @PostMapping
+  public Inventory create(@RequestBody Inventory inventory) {
+    return this.inventoryDAO.create(inventory);
   }
 
   /**

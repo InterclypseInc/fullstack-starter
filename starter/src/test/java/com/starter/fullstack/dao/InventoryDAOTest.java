be319e2bf00b7a137031ac36f2bad0044c5aa328
hendersonick <nickster9808@gmail.com>
2023-11-01 21:38:20 -0400

    Create and Delete Tests

--- starter/src/test/java/com/starter/fullstack/dao/InventoryDAOTest.java
+++ starter/src/test/java/com/starter/fullstack/dao/InventoryDAOTest.java
@@ -1,6 +1,8 @@
 package com.starter.fullstack.dao;
 
 import com.starter.fullstack.api.Inventory;
+import com.starter.fullstack.api.Product;
+
 import java.util.List;
 import javax.annotation.Resource;
 import org.junit.After;
@@ -52,4 +54,47 @@ public class InventoryDAOTest {
     List<Inventory> actualInventory = this.inventoryDAO.findAll();
     Assert.assertFalse(actualInventory.isEmpty());
   }
+
+
+  /**
+   * Testing the InventoryDAO Create method. First check to ensure only one inventory
+   * item is currently stored in the data base. Then, check to see if that one that 
+   * was added still has a null Mongo ID, or if it has been succesfully assigned.
+   */
+  @Test
+  public void createTest() {
+
+    Inventory inventory = new Inventory();
+    inventory.setName(NAME);
+    inventory.setProductType(PRODUCT_TYPE);
+    this.inventoryDAO.create(inventory);
+
+    Assert.assertEquals(1, this.mongoTemplate.findAll(Inventory.class).size());
+
+    Assert.assertNotEquals(null, this.mongoTemplate.findAll(Inventory.class).get(0).getId()); 
+  }
+
+  @Test
+  public void deleteTest() { 
+
+    Inventory inventory = new Inventory();
+    inventory.setName(NAME);
+    inventory.setProductType(PRODUCT_TYPE);
+    this.inventoryDAO.create(inventory);
+
+
+    /* Assuring that the inventory was sucessfully added to the Mongo Template */
+    Assert.assertEquals(1, this.mongoTemplate.findAll(Inventory.class).size());
+
+    /* Removing the previosuly added inventory from the MongoTemplate */
+    this.inventoryDAO.delete(inventory.getId());
+
+     /* Retrieving a list of all the inventory objects in the MongoTemplate (database) */
+     List<Inventory> actualInventory = this.inventoryDAO.findAll();
+
+     /* checking to ensure that the list is empty because calling delete() should remove the single element from the MongoTemplate(database) */
+     Assert.assertTrue(actualInventory.isEmpty());
+    
+  }
+
 }

98af9f3e46984d9d6e01129388ea4a14a88c73d4
hendersonick <nickster9808@gmail.com>
2023-11-06 22:28:12 -0500

    Task 1 InvDAO create and Test

--- starter/src/test/java/com/starter/fullstack/dao/InventoryDAOTest.java
+++ starter/src/test/java/com/starter/fullstack/dao/InventoryDAOTest.java
@@ -52,4 +52,23 @@ public class InventoryDAOTest {
     List<Inventory> actualInventory = this.inventoryDAO.findAll();
     Assert.assertFalse(actualInventory.isEmpty());
   }
+
+
+  /**
+   * Testing the InventoryDAO Create method. First check to ensure only one inventory
+   * item is currently stored in the data base. Then, check to see if that one that 
+   * was added still has a null Mongo ID, or if it has been succesfully assigned.
+   */
+  @Test
+  public void createTest() {
+
+    Inventory inventory = new Inventory();
+    inventory.setName(NAME);
+    inventory.setProductType(PRODUCT_TYPE);
+    this.inventoryDAO.create(inventory);
+
+    Assert.assertEquals(1, this.mongoTemplate.findAll(Inventory.class).size());
+
+    Assert.assertNotEquals(null, this.mongoTemplate.findAll(Inventory.class).get(0).getId()); 
+  }
 }
